a32a65aa2b814e72715cb74d58405cc757ba3e33
Rishabh Goel <Rishabh@rishabhs-mbp.lan>
2019-04-29 16:27:25 -0400

    got showProducts function to work on starting app

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,14 +4,27 @@ const inquirer = require("inquirer");
 const mysql = require("mysql");
 
 // establish database connection
-const database = mysql.createConnection({
+const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: process.env.DB_PASS,
   database: "bamazon"
 });
 // check for if database successfully connected 
-database.connect(function(err) {
+connection.connect(function(err) {
   if(err) throw err;
-  console.log("Connected as ID: " + database.threadId);
+  console.log("Connected as ID: " + connection.threadId);
 });
+
+var showProducts = function() {
+  connection.query("SELECT * FROM products", function(err, res) {
+    if(err) throw err;
+
+    for(var i = 0; i < res.length; i++) {
+      console.log("ID: " + res[i].id + " ||| " + "Product Name: " + res[i].product_name + " ||| " + "Department: " + res[i].dept_name + " ||| " + "Price: " + res[i].price + " ||| " + "QTY: " + res[i].stock_quant);
+    }
+    
+  });
+}
+
+showProducts();
